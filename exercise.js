ef09afe0c59d19e41a9d38f1336df8711422e5d6
Juan <jcarriola83@gmail.com>
2023-04-09 21:51:37 -0700

    Iteration through the array for collapsing the repeated values

--- exercise.js
+++ exercise.js
@@ -2,4 +2,26 @@ const Compresor = (string) => {
   let array = string.split("");
 
   onlyLettersArray = array.filter((item) => isNaN(item));
+
+  const colapsedValuesArray = [];
+  counter = 0;
+  newLetter = "";
+
+  for (let i = 0; i < onlyLettersArray.length; i++) {
+    if (onlyLettersArray[i] !== newLetter) {
+      if (counter > 1) {
+        colapsedValuesArray.push(counter);
+      }
+
+      counter = 1;
+
+      colapsedValuesArray.push(onlyLettersArray[i]);
+      newLetter = onlyLettersArray[i];
+    } else {
+      counter++;
+      if (i === onlyLettersArray.length - 1) {
+        colapsedValuesArray.push(counter);
+      }
+    }
+  }
 };
